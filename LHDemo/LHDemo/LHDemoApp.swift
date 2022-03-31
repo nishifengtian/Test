f2bc3b837542446b439c7cc38fdc6423101d8119
liuhui <17052709683@163.com>
2022-03-31 16:41:43 +0800

    New
    New

---
+++ LHDemo/LHDemo/LHDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  LHDemoApp.swift
+//  LHDemo
+//
+//  Created by 刘慧 on 2022/3/31.
+//
+
+import SwiftUI
+
+@main
+struct LHDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
